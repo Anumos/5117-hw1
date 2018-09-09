315c06333e594460a086d6d50642507954f8ba16
Michael Wang <michael.wang@ibm.com>
2018-09-08 21:58:23 -0700

    added form to take a comma separated string of values, added handler to render a bulleted list of those values.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Number Formatter' });
+});
+
+router.post('/format-numbers', function(req, res, next) {
+  var numbers = req.body.numbers;
+  console.log(numbers);
+  var nums = numbers.split(",");
+  res.render('format-numbers', 
+  	{  
+  		list : nums,
+  		title : "Numbers"
+  	});
 });
 
 module.exports = router;
